2bc54a1da6063935db4674d6373b149a57d9d50f
Brilliance Tech Sols <namanipradeepkumar@gmail.com>
2015-03-06 20:28:13 +0530

    Paypal Merchant email has been changed.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -63,7 +63,7 @@ class PaymentsController < ApplicationController
 
   def redirect_to_paypal
     values = {
-        business: "namanipradeepkumar-facilitator@gmail.com",
+        business: "merchant.brilliancetech@gmail.com",
         upload: 1,
         no_shipping: 1,
         return: "#{Rails.application.secrets.app_host}/payments",
